4cbce169529fde4176d085f92680911ad490cfad
MajnuDev <138400317+MajnuDev@users.noreply.github.com>
2023-07-10 04:01:02 +0530

    Create indre.js

---
+++ indre.js
@@ -0,0 +1,24 @@
+
+      var authM =localStorage.getItem("Auth");
+      if(authM==null || authM=="" || authM!="Monu Singh"){
+        window.location.href = "LogIn.html";
+      }
+
+      var UserLog = localStorage.getItem("user");
+     var Logout = document.getElementById("user");
+     Logout.textContent=UserLog;
+   Logout.addEventListener("click",function(){
+    localStorage.clear();
+   })
+
+        const input = document.getElementById("myInput");
+        const submitButton = document.querySelector(".sign-up");
+
+         function next() {
+            event.preventDefault();
+            localStorage.setItem("username", input.value);
+            window.location.href = "AddName.html"; // Redirect to the next page
+          
+        };
+
+        
